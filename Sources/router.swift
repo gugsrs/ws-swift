5ef72038efd4f371fe73aea3fe8640271eafaab1
Gustavo Siqueira <gus_rsiqueira@hotmail.com>
2016-07-07 16:21:10 -0300

    branch dev

--- Sources/router.swift
+++ Sources/router.swift
@@ -4,13 +4,16 @@ import Router
 import HTTP
 import Sideburns
 
-let router = Router { router in
-	router.get("/users") { request in
-		return try Response(status: .OK, templatePath:"Resources/template.html", templateData:"")
+let router = Router { route in
+	route.get("/users") { request in
+		return try Response(status: .OK, templatePath:"Resources/templates/index.html", templateData:"")
 	}
 
-	router.get("/users/:id") { request in
+	route.get("/users/:id") { request in
 		let id = request.parameters["id"]
 		return Response(status: .OK, json: JSON.from(id!))
 	}
+
+    let staticFiles = FileResponder(basePath: "Resources/static/")
+    route.fallback(staticFiles)
 }
